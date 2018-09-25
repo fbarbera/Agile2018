da20bbd97e03e41b4b4eecf93182f8ee79afef9d
Facundo Barbera <facundo.barbera@globant.com>
2018-09-24 21:06:35 -0300

    add javascript

---
+++ script/altaReclamo.js
@@ -0,0 +1,63 @@
+function submit() {
+    var database = firebase.database();
+
+    var calle = document.getElementById('calleId');
+    var id_calle = calle.options[calle.selectedIndex].value;
+    var entre1 = document.getElementById('entre1Id');
+    var id_entre1 = entre1.options[entre1.selectedIndex].value;
+    var entre2 = document.getElementById('entre2Id');
+    var id_entre2 = entre2.options[entre2.selectedIndex].value;
+    var altura = document.getElementById('alturaId');
+    var id_altura = altura.options[altura.selectedIndex].value;
+    var descripcion = document.getElementById('descripcionId');
+    var id_descripcion = descripcion.options[descripcion.selectedIndex].value;
+    var contacto = document.getElementById('contactoId');
+    var id_contacto = contacto.options[contacto.selectedIndex].value;
+    var comentario = document.getElementById('comentarioId');
+    var id_comentario = comentario.options[comentario.selectedIndex].value;
+
+    //alert(id_usuario);
+    //alert(id_puerta);
+
+    /*var datos = new Object();
+	datos.listaUsu = usuario;
+	datos.listaPue = puerta;*/
+
+
+    var KeyUsu = "";
+    var KeyPue = "";
+
+    var rtaMainUsu = database.ref('reclamos');
+    rtaMainUsu.orderByChild("calle").equalTo(id_calle).on('child_added', function (ss) {
+        var rtaMainUsu = ss.val();
+        rtaMainUsu.key = ss.key;
+        KeyUsu = rtaMainUsu.key;
+        //alert(KeyUsu);
+    });
+
+    var rtaMainPue = database.ref('Puerta');
+    rtaMainPue.orderByChild("Descripcion").equalTo(id_puerta).on('child_added', function (sss) {
+        var rtaMainPue = sss.val();
+        rtaMainPue.key = sss.key;
+
+        KeyPue = rtaMainPue.Key;
+
+        alert(KeyPue);
+
+        //console.log(KeyPue);
+    });
+
+    alert(KeyPue);
+    alert(KeyUsu);
+	/*
+	if(KeyPue =  KeyUsu)
+	{
+		alert("ok");
+	}
+	else 
+	{
+		alert("NOOOOOO");
+	}
+	*/
+
+}
