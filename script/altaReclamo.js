89eb475787b345a7196f644386fe65ed67b544ad
Facundo Barbera <facundo.barbera@globant.com>
2018-09-25 10:26:12 -0300

    cambio para conectar firabase.  Mejoras esteticas y de logica js

--- script/altaReclamo.js
+++ script/altaReclamo.js
@@ -1,89 +1,57 @@
-function submit() {
+function save() {
  
     var database = firebase.database();
 
-    var calle = document.getElementById('calleId');
-    var id_calle = calle.options[calle.selectedIndex].value;
-    var entre1 = document.getElementById('entre1Id');
-    var id_entre1 = entre1.options[entre1.selectedIndex].value;
-    var entre2 = document.getElementById('entre2Id');
-    var id_entre2 = entre2.options[entre2.selectedIndex].value;
-    var altura = document.getElementById('alturaId');
-    var id_altura = altura.options[altura.selectedIndex].value;
-    var descripcion = document.getElementById('descripcionId');
-    var id_descripcion = descripcion.options[descripcion.selectedIndex].value;
-    var contacto = document.getElementById('contactoId');
-    var id_contacto = contacto.options[contacto.selectedIndex].value;
+    var calle = document.getElementById('calleId').value;   
+    var entre1 = document.getElementById('entre1Id').value;
+    var entre2 = document.getElementById('entre2Id').value;    
+    var altura = document.getElementById('alturaId').value;    
+    var asunto = document.getElementById('asuntoId');
+    var id_asunto = asunto.options[asunto.selectedIndex].value;
+    var contacto = document.getElementById('contactoId').value;    
     var comentario = document.getElementById('comentarioId');
-    var id_comentario = comentario.options[comentario.selectedIndex].value;
+   
+    var KeyRec = "";    
 
-    alert(id_comentario);
-    
-
-
-    var KeyUsu = "";
-    var KeyPue = "";
-
-    var rtaMainUsu = database.ref('reclamos');
-    rtaMainUsu.orderByChild("calle").equalTo(id_calle).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    var rtaSaveRec = database.ref('reclamos');
+    rtaSaveRec.orderByChild("calle").equalTo(calle).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("entre1").equalTo(id_entre1).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    rtaSaveRec.orderByChild("entre1").equalTo(entre1).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("entre2").equalTo(id_entre2).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    rtaSaveRec.orderByChild("entre2").equalTo(entre2).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("altura").equalTo(id_altura).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    rtaSaveRec.orderByChild("altura").equalTo(altura).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("descripcion").equalTo(id_descripcion).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    rtaSaveRec.orderByChild("asunto").equalTo(id_asunto).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("contacto").equalTo(id_contacto).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
+    rtaSaveRec.orderByChild("contacto").equalTo(contacto).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
     });
-    rtaMainUsu.orderByChild("comentario").equalTo(id_comentario).on('child_added', function (ss) {
-        var rtaMainUsu = ss.val();
-        rtaMainUsu.key = ss.key;
-        KeyUsu = rtaMainUsu.key;
-        //alert(KeyUsu);
-    });
-
-    var rtaMainPue = database.ref('Puerta');
-    rtaMainPue.orderByChild("Descripcion").equalTo(id_puerta).on('child_added', function (sss) {
-        var rtaMainPue = sss.val();
-        rtaMainPue.key = sss.key;
-
-        KeyPue = rtaMainPue.Key;
-
-        alert(KeyPue);
-
-        //console.log(KeyPue);
-    });
-
-    alert(KeyPue);
-    alert(KeyUsu);
+    rtaSaveRec.orderByChild("comentario").equalTo(comentario).on('child_added', function (ss) {
+        var rtaSaveRec = ss.val();
+        rtaSaveRec.key = ss.key;
+        KeyRec = rtaSaveRec.key;        
+    });  
+    alert(KeyRec);
 	/*
-	if(KeyPue =  KeyUsu)
+	if(KeyPue =  KeyRec)
 	{
 		alert("ok");
 	}
