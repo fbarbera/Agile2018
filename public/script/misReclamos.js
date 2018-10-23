52f33172ed7bf481f7a11e88b19fc22409fb43c2
fbarbera <34081993+fbarbera@users.noreply.github.com>
2018-10-22 22:11:21 -0300

    Se completa el combo de reclamos por userID

--- public/script/misReclamos.js
+++ public/script/misReclamos.js
@@ -2,6 +2,7 @@ var selectReclamos = document.getElementById("reclamosId");
 obtenerReclamos();
 
 function obtenerReclamos() {
+  var database = firebase.database();
   firebase.auth().signInAnonymously().catch(function (error) {
     // Handle Errors here.
     var errorCode = error.code;
@@ -10,20 +11,37 @@ function obtenerReclamos() {
     // ...
   });
   var datos;
-  firebase.auth().onAuthStateChanged({
-    function(_userId) {
+  
+  firebase.auth().onAuthStateChanged(function(_userId) {
       var userId = _userId.uid;
       console.log(userId);
       if (userId) {
-        firebase.database().ref('/reclamos/' + userId).once('value').then(function (snapshot) {
+        /*firebase.database().ref('/reclamos/' + userId).once('value').then(function (snapshot) {
           datos = (snapshot.val() && snapshot.val().userId) || userId;
           alert(datos);
+          });*/
+        var keyRec;
+        var reclamosCol = database.ref('reclamos');
+        reclamosCol.orderByChild("userId").equalTo(userId).on('child_added', function (ss) {
+          var reclamosCol = ss.val();
+          reclamosCol.key = ss.key;
+          //console.log(reclamos)
+          var reclamosArray = [];
+          reclamosArray.splice(0, reclamosArray.length, {altura: reclamosCol.altura, nroReclamo: reclamosCol.nroReclamo});
+          console.log(reclamosArray);
+          KeyRec = reclamosCol.key;
+          for (i in reclamosArray) {
+            var optionRec = document.createElement("option");
+            optionRec.value = reclamosArray[i].altura;
+            optionRec.text = reclamosArray[i].altura + ' - ' + reclamosArray[i].nroReclamo;
+            selectReclamos.appendChild(optionRec);
+          }
         });
       } else {
         console.log("error user");
       }// ...     
-    }
+    
   });
-  console.log(datos);
+  //console.log(datos);
 
 }
