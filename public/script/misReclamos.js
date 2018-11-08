8087164fbd259125418e69222a7757dba6d5ff80
fbarbera <34081993+fbarbera@users.noreply.github.com>
2018-11-08 19:58:06 -0300

    buscar bonito

--- public/script/misReclamos.js
+++ public/script/misReclamos.js
@@ -1,5 +1,8 @@
 var selectReclamos = document.getElementById("reclamosId");
+//selectReclamos.onclick=irAlReclamo(selectReclamos.options[selectReclamos.selectedIndex].value);
 obtenerReclamos();
+
+
 
 function obtenerReclamos() {
   var database = firebase.database();
@@ -11,37 +14,43 @@ function obtenerReclamos() {
     // ...
   });
   var datos;
-  
-  firebase.auth().onAuthStateChanged(function(_userId) {
-      var userId = _userId.uid;
-      console.log(userId);
-      if (userId) {
-        /*firebase.database().ref('/reclamos/' + userId).once('value').then(function (snapshot) {
-          datos = (snapshot.val() && snapshot.val().userId) || userId;
-          alert(datos);
-          });*/
-        var keyRec;
-        var reclamosCol = database.ref('reclamos');
-        reclamosCol.orderByChild("userId").equalTo(userId).on('child_added', function (ss) {
-          var reclamosCol = ss.val();
-          reclamosCol.key = ss.key;
-          //console.log(reclamos)
-          var reclamosArray = [];
-          reclamosArray.splice(0, reclamosArray.length, {altura: reclamosCol.altura, nroReclamo: reclamosCol.nroReclamo});
-          console.log(reclamosArray);
-          KeyRec = reclamosCol.key;
-          for (i in reclamosArray) {
-            var optionRec = document.createElement("option");
-            optionRec.value = reclamosArray[i].altura;
-            optionRec.text = reclamosArray[i].altura + ' - ' + reclamosArray[i].nroReclamo;
-            selectReclamos.appendChild(optionRec);
-          }
-        });
-      } else {
-        console.log("error user");
-      }// ...     
-    
+
+  firebase.auth().onAuthStateChanged(function (_userId) {
+    var userId = _userId.uid;
+    console.log(userId);
+    if (userId) {
+      /*firebase.database().ref('/reclamos/' + userId).once('value').then(function (snapshot) {
+        datos = (snapshot.val() && snapshot.val().userId) || userId;
+        alert(datos);
+        });*/
+      var keyRec;
+      var reclamosCol = database.ref('reclamos');
+      reclamosCol.orderByChild("userId").equalTo(userId).on('child_added', function (ss) {
+        var reclamosCol = ss.val();
+        reclamosCol.key = ss.key;
+        var reclamosArray = [];
+        reclamosArray.splice(0, reclamosArray.length, { 
+          categoria: reclamosCol.categoria, 
+          nroReclamo: reclamosCol.nroReclamo, 
+          estado: reclamosCol.estado });
+        KeyRec = reclamosCol.key;
+        for (i in reclamosArray) {
+          var optionRec = document.createElement("option");
+          optionRec.value = reclamosArray[i].nroReclamo;
+          optionRec.text =  ' Reclamo numero: ' + reclamosArray[i].nroReclamo + ' - Estado: ' + reclamosArray[i].estado;
+          selectReclamos.appendChild(optionRec);
+        }
+      });
+    } else {
+      console.log("error user");
+    }// ...     
+
   });
   //console.log(datos);
-
 }
+
+
+selectReclamos.addEventListener('change', function () {
+  var index = selectReclamos.value;
+  console.log(index);
+})
